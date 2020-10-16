4b128eeb0d603c4960a4f98281e448e48e3e1c3d
Robustoe <aligazitepe@gmail.com>
2020-10-16 14:39:50 +0200

    chore:deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setAllBeers } from "./redux/beer/beerSlice";
 
 import BeerList from "./Components/BeerList/BeerList";
 import Navbar from "./Components/Navbar/Navbar";
-import Projects from "./Components/Projects/Projects";
 
 import "./App.css";
 
@@ -15,14 +14,14 @@ import ApiClient from "./Services/ApiClient";
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    ApiClient.getallBeers().then(res=> dispatch(setAllBeers(res)))
+    ApiClient.getallBeers().then((res) => dispatch(setAllBeers(res)));
   }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-       <BeerList />
+        <BeerList />
       </div>
     </BrowserRouter>
   );
